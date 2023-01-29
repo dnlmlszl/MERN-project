c1b73cde609bd03cfa77d06463cf93fe5a795fda
László Mihály Dániel <daniel.mlaszlo@yahoo.com>
2023-01-29 12:50:59 +0100

    updating repo with changes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // Middlewares
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateUser from "./middleware/auth.js"
 
 const app = express();
 dotenv.config();
@@ -30,7 +31,7 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
